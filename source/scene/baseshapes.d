22dff57875d75250c4712a7f698d898ee158a316
Crom (Thibaut CHARLES) <crom29@hotmail.fr>
2014-06-04 00:34:30 +0200

    Added VBO to all basicshapes

--- source/scene/baseshapes.d
+++ source/scene/baseshapes.d
@@ -153,14 +153,6 @@ class Crate : Cube {
 		enum tl = [0.0,1.0];
 		enum tr = [1.0,1.0];
 
-		//m_vertices = 
-		//	 A~B~C~A~C~D
-		//	~B~F~G~B~G~C
-		//	~A~E~F~A~F~B
-		//	~E~H~G~E~G~F
-		//	~D~C~G~D~G~H
-		//	~E~A~D~E~D~H;
-
 		m_texturecoord = 
 			 tr~br~bl~tr~bl~tl
 			~br~tr~tl~br~tl~bl
@@ -171,17 +163,27 @@ class Crate : Cube {
 
 		m_texture = Resource.Get!Texture("crate.jpg").id;
 		m_prog = Resource.Get!Program("texture.prg").id;
+
+		try{
+			m_vbotext = Resource.Get!Vbo("CrateText");
+		}
+		catch(ResourceException e){
+			m_vbotext = Resource.CreateRes!Vbo("CrateText", Vbo.Rate.Rarely, m_texturecoord);
+		}
 	}
 
 	override void Render(ref mat4 proj, ref mat4 mdlview){
 		glUseProgram(m_prog);
 
-			glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, m_vertices.ptr);
-			glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, m_colors.ptr);
-			glVertexAttribPointer(2, 2, GL_FLOAT, false, 0, m_texturecoord.ptr);
-			glEnableVertexAttribArray(0);
-			glEnableVertexAttribArray(1);
-			glEnableVertexAttribArray(2);
+			glBindBuffer(GL_ARRAY_BUFFER, m_vbo.id);
+				glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, cast(void*)(m_vbo.offset[0]));
+				glVertexAttribPointer(1, 3, GL_FLOAT, false, 0, cast(void*)(m_vbo.offset[1]));
+				glEnableVertexAttribArray(0);
+				glEnableVertexAttribArray(1);
+			glBindBuffer(GL_ARRAY_BUFFER, m_vbotext.id);
+				glVertexAttribPointer(2, 2, GL_FLOAT, false, 0, cast(void*)(m_vbotext.offset[0]));
+				glEnableVertexAttribArray(2);
+			glBindBuffer(GL_ARRAY_BUFFER, 0);
 
 			glUniformMatrix4fv(glGetUniformLocation(m_prog, "projection"), 1, true, proj.value_ptr);
 			glUniformMatrix4fv(glGetUniformLocation(m_prog, "modelview"), 1, true, mdlview.value_ptr);
@@ -192,6 +194,7 @@ class Crate : Cube {
 
 			glBindTexture(GL_TEXTURE_2D, 0);
 
+			glDisableVertexAttribArray(2);
 			glDisableVertexAttribArray(1);
 			glDisableVertexAttribArray(0);
 
@@ -200,4 +203,5 @@ class Crate : Cube {
 
 	uint m_texture;
 	float m_texturecoord[];
+	Vbo m_vbotext;
 }
